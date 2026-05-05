f01259a0945501f3b13b51c8be562def8159e7ac
ChrisWilloughby <cwilloughby132@gmail.com>
2026-05-05 14:04:25 -0400

    fixing links for windows

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { CssBaseline } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ToasterProvider from './providers/ToasterProvider';
